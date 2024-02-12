ed1d0ac2a0537bdcb366196dd2e112008112cf37
Vladimir <senyuko2001@gmail.com>
2024-02-12 12:39:25 +0300

    initial commit

---
+++ tz_app/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace tz_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 18);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(408, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 45);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(941, 419);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(878, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Поиск объектов";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(646, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(209, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(855, 477);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Количество строк";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Просмотр таблиц";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(643, 2);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Поиск объектов";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 472);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Выгрузить в excel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 499);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ tz_app/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace tz_app
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection sqlConnection;
+        private SqlDataAdapter sqlDataAdapter;
+        private DataTable dataTable;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            sqlConnection = new SqlConnection("Data Source=DESKTOP-97LV1Q7\\SQLEXPRESS;Initial Catalog=intermech;Integrated Security=True;TrustServerCertificate=True");
+
+            FillComboBox();
+
+            comboBox1.SelectedIndexChanged += (s, e) =>
+            {
+                if (comboBox1.SelectedItem != null)
+                {
+                    FillDataGridView();
+                }
+            };
+        }
+
+        private void FillComboBox()
+        {
+            // using для автоматического закрытия соединений
+            using (SqlConnection conn1 = new SqlConnection("Data Source=DESKTOP-97LV1Q7\\SQLEXPRESS;Initial Catalog=intermech;Integrated Security=True;TrustServerCertificate=True"))
+            {
+                conn1.Open();
+                SqlCommand sqlCommand = new SqlCommand("SELECT TABLE_NAME FROM intermech.INFORMATION_SCHEMA.TABLES", conn1);
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+                ArrayList list = new ArrayList();
+                while (sqlDataReader.Read())
+                {
+                    string tableName = sqlDataReader[0].ToString();
+                    // Два соединения, одно для чтения имен таблиц, а другое для подсчета строк 
+                    using (SqlConnection conn2 = new SqlConnection("Data Source=DESKTOP-97LV1Q7\\SQLEXPRESS;Initial Catalog=intermech;Integrated Security=True;TrustServerCertificate=True"))
+                    {
+                        conn2.Open();
+                        SqlCommand countCommand = new SqlCommand($"SELECT COUNT(*) FROM [{tableName}]", conn2);
+                        int rowCount = (int)countCommand.ExecuteScalar();
+                        if (rowCount > 0)
+                        {
+                            list.Add(tableName);
+                        }
+                    }
+                }
+                list.Sort();
+                foreach (var item in list)
+                {
+                    comboBox1.Items.Add(item);
+                }
+            }
+        }
+
+        private void FillDataGridView()
+        {
+            button2.Visible = false;
+            sqlDataAdapter = new SqlDataAdapter($"SELECT * FROM [{comboBox1.SelectedItem}]", sqlConnection);
+            dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+
+
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand($"SELECT COUNT(*) FROM [{comboBox1.SelectedItem}]", sqlConnection);
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+            while (sqlDataReader.Read())
+            {
+                label1.Text = "Всего: " + sqlDataReader[0].ToString();
+            }
+            sqlConnection.Close();
+        }
+
+        private void SearchAndFillDataGridView(string searchValue)
+        {
+            string query = "SELECT R.F_PROJ_ID, O.F_OBJECT_ID, R.F_RELATION_TYPE, T.F_DESCRIPTION, T.F_TYPE_NAME, T.F_NOTE, O.F_OBJECT_TYPE, OT.F_OBJ_TYPE_NAME, A.F_ATTRIBUTE_LIST " +
+                           "FROM [IMS_RELATIONS] R " +
+                           "INNER JOIN [IMS_RELATION_TYPES] T ON R.F_RELATION_TYPE = T.F_RELATION_TYPE " +
+                           "INNER JOIN [IMS_OBJECTS] O ON R.F_PART_ID = O.F_ID " +
+                           "INNER JOIN [IMS_OBJECT_TYPES] OT ON O.F_OBJECT_TYPE = OT.F_OBJECT_TYPE " +
+                           "LEFT JOIN (" +
+                           "    SELECT AO.F_OBJECT_ID, STRING_AGG(AT.F_NAME, ', ') AS F_ATTRIBUTE_LIST " +
+                           "    FROM [IMS_OBJECT_ATTRS] AO " +
+                           "    INNER JOIN [IMS_ATTRIBUTES] AT ON AO.F_ATTRIBUTE_ID = AT.F_ATTRIBUTE_ID " +
+                           "    GROUP BY AO.F_OBJECT_ID " +
+                           ") A ON O.F_OBJECT_ID = A.F_OBJECT_ID " +
+                           "WHERE R.[F_PROJ_ID] = @searchValue";
+
+            SqlCommand command = new SqlCommand(query, sqlConnection);
+            command.Parameters.AddWithValue("@searchValue", searchValue);
+
+            sqlDataAdapter = new SqlDataAdapter(command);
+            dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+            sqlConnection.Close();
+
+
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand($"SELECT COUNT(*) FROM [IMS_RELATIONS] WHERE [F_PROJ_ID] = '{searchValue}'", sqlConnection);
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+            while (sqlDataReader.Read())
+            {
+                label1.Text = "Всего: " + sqlDataReader[0].ToString();
+            }
+            sqlConnection.Close();
+            button2.Visible = true;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox2.Text))
+            {
+                SearchAndFillDataGridView(textBox2.Text);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Microsoft.Office.Interop.Excel._Application excel = new Microsoft.Office.Interop.Excel.Application();
+
+            Microsoft.Office.Interop.Excel._Workbook workbook = excel.Workbooks.Add(Type.Missing);
+
+            Microsoft.Office.Interop.Excel._Worksheet worksheet = null;
+            worksheet = workbook.Sheets["Лист1"];
+            worksheet = workbook.ActiveSheet;
+            worksheet.Name = "Объект";
+
+            for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
+            {
+                worksheet.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
+            }
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    if (dataGridView1.Rows[i].Cells[j].Value != null)
+                    {
+                        worksheet.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                    }
+                    else
+                    {
+                        worksheet.Cells[i + 2, j + 1] = "";
+                    }
+                }
+            }
+
+
+            dataGridView1.Columns["F_PROJ_ID"].HeaderText = "Айди родительского объекта";
+            dataGridView1.Columns["F_OBJECT_ID"].HeaderText = "Дочерние объекты";
+            dataGridView1.Columns["F_RELATION_TYPE"].HeaderText = "Номер типа связи";
+            dataGridView1.Columns["F_DESCRIPTION"].HeaderText = "Описание типа связи";
+            dataGridView1.Columns["F_TYPE_NAME"].HeaderText = "Тип связи";
+            dataGridView1.Columns["F_NOTE"].HeaderText = "Примечание к типу связи";
+            dataGridView1.Columns["F_OBJECT_TYPE"].HeaderText = "Номер типа объекта";
+            dataGridView1.Columns["F_OBJ_TYPE_NAME"].HeaderText = "Имя объекта";
+            dataGridView1.Columns["F_ATTRIBUTE_LIST"].HeaderText = "Список атрибутов объекта";
+
+            for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
+            {
+                worksheet.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
+            }
+
+
+            excel.DisplayAlerts = false;
+            Microsoft.Office.Interop.Excel.Range range = worksheet.Range[worksheet.Cells[2, 1], worksheet.Cells[dataGridView1.Rows.Count + 1, 1]];
+            range.Merge();
+            excel.DisplayAlerts = true;
+
+
+            worksheet.Columns.AutoFit();
+            worksheet.Range["A:I"].HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+            worksheet.Range["A:I"].VerticalAlignment = Microsoft.Office.Interop.Excel.XlVAlign.xlVAlignCenter;
+
+            worksheet.Columns["F:F"].ColumnWidth = 60;
+            worksheet.Columns["I:I"].ColumnWidth = 60; 
+
+            worksheet.Columns["F:F"].WrapText = true; 
+            worksheet.Columns["I:I"].WrapText = true; 
+
+            worksheet.Columns["F:F"].EntireRow.AutoFit();
+            worksheet.Columns["I:I"].EntireRow.AutoFit();
+
+
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "Excel Files|*.xlsx";
+            saveDialog.Title = "Сохранить как Excel файл";
+            saveDialog.ShowDialog();
+
+            if (saveDialog.FileName != "")
+            {
+                workbook.SaveAs(saveDialog.FileName);
+            }
+
+            excel.Quit();
+        }
+    }
+}
